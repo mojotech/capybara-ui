c118ec10500ef379e0de3fffae9e38d603debc3d
David Leal <dleal@mojotech.com>
2013-07-14 18:58:37 +0100

    Define Document apart from Widget.

--- lib/cucumber/salad/widgets/document.rb
+++ lib/cucumber/salad/widgets/document.rb
@@ -1,22 +1,20 @@
 module Cucumber
   module Salad
     module Widgets
-      class Document < Widget
+      class Document
         include WidgetContainer
-
-        root 'body'
 
         def initialize(options)
           self.widget_lookup_scope =
             options.delete(:widget_lookup_scope) or raise "No scope given"
-
-          options[:root] ||= Capybara.current_session
-
-          super options
         end
 
         def widget(name)
           widget_class(name).in_node(root)
+        end
+
+        def root
+          Capybara.current_session
         end
       end
     end
