ac2e5d1460c8749a5791aae05b54b5f9ef34bc32
David Leal <dleal@mojotech.com>
2013-07-27 11:55:54 +0100

    [spec] Require capybara/rspec instead of just capybara.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,7 @@ $:.unshift File.expand_path(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'cucumber/salad'
 require 'rspec/given'
 require 'sinatra/base'
-require 'capybara'
+require 'capybara/rspec'
 
 class SaladApp < Sinatra::Base; end
 Capybara.app = SaladApp
