1cef2bbc0bda3b3ed6fa201819bafcff39fd5cc6
David Leal <dleal@mojotech.com>
2013-07-28 12:58:28 +0100

    [form] Let field groups exist apart from forms.

---
+++ lib/cucumber/salad/widgets/field_group.rb
@@ -0,0 +1,71 @@
+module Cucumber
+  module Salad
+    module Widgets
+      # A group of form fields.
+      class FieldGroup < Widget
+        root 'fieldset'
+
+        def self.default_locator(type = nil, &block)
+          alias_method :name_to_locator, type if type
+
+          define_method :name_to_locator, &block if block
+        end
+
+        def self.check_box(name, label = nil)
+          define_method "#{name}=" do |val|
+            l = label || name_to_locator(name)
+
+            if val
+              root.check l
+            else
+              root.uncheck l
+            end
+          end
+        end
+
+        def self.select(name, *args)
+          opts   = args.extract_options!
+          label, = args
+
+          define_method "#{name}=" do |val|
+            l = label || name_to_locator(name)
+            w = opts.fetch(:writer) { ->(v) { v } }
+
+            root.select w.(val).to_s, from: l
+          end
+        end
+
+        def self.text_field(name, label = nil)
+          define_method "#{name}=" do |val|
+            l = label || name_to_locator(name)
+
+            root.fill_in l, with: val.to_s
+          end
+        end
+
+        # Sets the given form attributes.
+        #
+        # @param attributes [Hash] the attributes and values we want to set.
+        #
+        # @return the current widget.
+        def set(attributes)
+          attributes.each do |k, v|
+            send "#{k}=", v
+          end
+
+          self
+        end
+
+        private
+
+        def label(name)
+          name.to_s.humanize
+        end
+
+        def name_to_locator(name)
+          label(name)
+        end
+      end
+    end
+  end
+end
--- lib/cucumber/salad/widgets/form.rb
+++ lib/cucumber/salad/widgets/form.rb
@@ -1,59 +1,8 @@
 module Cucumber
   module Salad
     module Widgets
-      class Form < Widget
-        def self.default_locator(type = nil, &block)
-          alias_method :name_to_locator, type if type
-
-          define_method :name_to_locator, &block if block
-        end
-
-        def self.check_box(name, label = nil)
-          define_method "#{name}=" do |val|
-            l = label || name_to_locator(name)
-
-            if val
-              root.check l
-            else
-              root.uncheck l
-            end
-          end
-        end
-
-        def self.select(name, *args)
-          opts   = args.extract_options!
-          label, = args
-
-          define_method "#{name}=" do |val|
-            l = label || name_to_locator(name)
-            w = opts.fetch(:writer) { ->(v) { v } }
-
-            root.select w.(val).to_s, from: l
-          end
-        end
-
-        def self.text_field(name, label = nil)
-          define_method "#{name}=" do |val|
-            l = label || name_to_locator(name)
-
-            root.fill_in l, with: val.to_s
-          end
-        end
-
+      class Form < FieldGroup
         action :submit, '[type = submit]'
-
-        # Sets the given form attributes.
-        #
-        # @param attributes [Hash] the attributes and values we want to set.
-        #
-        # @return the current widget.
-        def set(attributes)
-          attributes.each do |k, v|
-            send "#{k}=", v
-          end
-
-          self
-        end
 
         # Submit form with +attributes+.
         #
@@ -63,16 +12,6 @@ module Cucumber
         def submit_with(attributes)
           set attributes
           submit
-        end
-
-        private
-
-        def label(name)
-          name.to_s.humanize
-        end
-
-        def name_to_locator(name)
-          label(name)
         end
       end
     end
