2bfb5e8be7fbfb7a9540aa47120b1689fde2aeab
David Leal <dleal@mojotech.com>
2013-07-29 23:13:22 +0100

    Separate HTML and container in field group specs.

--- spec/cucumber/salad/widgets/field_group_spec.rb
+++ spec/cucumber/salad/widgets/field_group_spec.rb
@@ -1,16 +1,54 @@
 require 'spec_helper'
 
 describe Cucumber::Salad::Widgets::FieldGroup do
-  GivenHTML <<-HTML
-    <form>
+  describe '.check_box' do
+    GivenHTML <<-HTML
       <p>
         <label for="cb">Unchecked box</label>
         <input type="checkbox" name="ub" id="ub">
       </p>
       <p>
-        <label for="t">Empty field</label>
-        <input type="text" name="ef" id="ef">
+        <label for="cb">Checked box</label>
+        <input type="checkbox" name="cb" id="cb" checked>
       </p>
+    HTML
+
+    Given(:container_class) { CheckBoxGroup }
+
+    class CheckBoxGroup < Cucumber::Salad::Widgets::FieldGroup
+      check_box :unchecked_box, 'ub'
+      check_box :checked_box, 'cb'
+    end
+
+    context "when defining" do
+      Then { CheckBoxGroup.field_names.include?(:unchecked_box) }
+    end
+
+    context "when querying" do
+      Then { container.checked_box == true }
+      Then { container.unchecked_box == false }
+    end
+
+    context "when setting" do
+      When { container.checked_box   = false }
+      When { container.unchecked_box = true }
+
+      Then { container.checked_box   == false }
+      Then { container.unchecked_box == true }
+    end
+
+    context 'when transforming to table' do
+      Given(:headers) {['unchecked box', 'checked box']}
+      Given(:values)  {['no', 'yes']}
+
+      When(:table)   { container.to_table }
+
+      Then { table == [headers, values] }
+    end
+  end
+
+  describe '.select' do
+    GivenHTML <<-HTML
       <p>
         <label for="d">Deselected</label>
         <select name="d" id="d">
@@ -19,100 +57,90 @@ describe Cucumber::Salad::Widgets::FieldGroup do
         </select>
       </p>
       <p>
-        <label for="cb">Checked box</label>
-        <input type="checkbox" name="cb" id="cb" checked>
-      </p>
-      <p>
-        <label for="ff">Filled field</label>
-        <input type="text" name="ff" id="ff" value="Field contents">
-      </p>
-      <p>
         <label for="s">Selected</label>
         <select name="s" id="s">
           <option selected>Selected option</option>
           <option>Unselected option</option>
         </select>
       </p>
-    </form>
-  HTML
+    HTML
 
-  class TestGroup < Cucumber::Salad::Widgets::FieldGroup
-    root 'form'
+    Given(:container_class) { SelectGroup }
 
-    check_box  :unchecked_box, 'ub'
-    select     :deselected, 'd'
-    text_field :empty_field, 'ef'
-    check_box  :checked_box, 'cb'
-    select     :selected, 's'
-    text_field :filled_field, 'ff'
-  end
+    class SelectGroup < Cucumber::Salad::Widgets::FieldGroup
+      select :deselected, 'd'
+      select :selected, 's'
+    end
 
-  Given(:root)  { find('form') }
-  Given(:group) { TestGroup.new(root: root) }
-
-  describe '.check_box' do
     context "when defining" do
-      Then { TestGroup.field_names.include?(:unchecked_box) }
+      Then { SelectGroup.field_names.include?(:deselected) }
     end
 
     context "when querying" do
-      Then { group.checked_box == true }
-      Then { group.unchecked_box == false }
+      Then { container.deselected.nil? }
+      Then { container.selected == "Selected option" }
     end
 
     context "when setting" do
-      When { group.checked_box   = false }
-      When { group.unchecked_box = true }
+      When { container.selected   = "Unselected option" }
+      When { container.deselected = "One" }
 
-      Then { group.checked_box   == false }
-      Then { group.unchecked_box == true }
-    end
-  end
-
-  describe '.select' do
-    context "when defining" do
-      Then { TestGroup.field_names.include?(:deselected) }
+      Then { container.selected   == "Unselected option" }
+      Then { container.deselected == "One" }
     end
 
-    context "when querying" do
-      Then { group.deselected.nil? }
-      Then { group.selected == "Selected option" }
-    end
+    context 'when transforming to table' do
+      Given(:headers) {['deselected', 'selected']}
+      Given(:values)  {['', 'selected option']}
 
-    context "when setting" do
-      When { group.selected   = "Unselected option" }
-      When { group.deselected = "One" }
+      When(:table)   { container.to_table }
 
-      Then { group.selected   == "Unselected option" }
-      Then { group.deselected == "One" }
+      Then { table == [headers, values] }
     end
   end
 
   describe '.text_field' do
+    GivenHTML <<-HTML
+      <p>
+        <label for="t">Empty field</label>
+        <input type="text" name="ef" id="ef">
+      </p>
+      <p>
+        <label for="ff">Filled field</label>
+        <input type="text" name="ff" id="ff" value="Field contents">
+      </p>
+    HTML
+
+    Given(:container_class) { TextGroup }
+
+    class TextGroup < Cucumber::Salad::Widgets::FieldGroup
+      text_field :empty_field, 'ef'
+      text_field :filled_field, 'ff'
+    end
+
     context "when defining" do
-      Then { TestGroup.field_names.include?(:empty_field) }
+      Then { TextGroup.field_names.include?(:empty_field) }
     end
 
     context "when querying" do
-      Then { group.empty_field.nil? }
-      Then { group.filled_field == "Field contents" }
+      Then { container.empty_field.nil? }
+      Then { container.filled_field == "Field contents" }
     end
 
     context "when setting" do
-      When { group.empty_field  = "Some text" }
-      When { group.filled_field = nil }
+      When { container.empty_field  = "Some text" }
+      When { container.filled_field = nil }
 
-      Then { group.empty_field  == "Some text" }
-      Then { group.filled_field == "" }
+      Then { container.empty_field  == "Some text" }
+      Then { container.filled_field == "" }
     end
-  end
 
-  describe '#to_table' do
-    Given(:table)   { group.to_table }
-    Given(:headers) {['unchecked box', 'deselected', 'empty field',
-                      'checked box', 'selected', 'filled field']}
-    Given(:values)  {['no', '', '', 'yes', 'selected option', 'field contents']}
+    describe '#to_table' do
+      Given(:table)   { container.to_table }
+      Given(:headers) {['empty field', 'filled field']}
+      Given(:values)  {['', 'field contents']}
 
-    Then { table == [headers, values] }
+      Then { table == [headers, values] }
+    end
   end
 end
