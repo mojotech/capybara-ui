982d62004fa288190e0e5f85b66477d33c0a81bb
David Leal <dleal@mojotech.com>
2013-09-08 16:06:06 +0100

    [widget] Fix indentation.

--- lib/dill/widget.rb
+++ lib/dill/widget.rb
@@ -8,169 +8,169 @@ module Dill
 
     # @!group Widget macros
 
-      # Defines a new action.
-      #
-      # This is a shortcut to help defining a widget and a method that clicks
-      # on that widget. You can then send a widget instance the message given
-      # by +name+.
-      #
-      # You can access the underlying widget by appending "_widget" to the
-      # action name.
-      #
-      # @example
-      #   # Consider the widget will encapsulate the following HTML
-      #   #
-      #   # <div id="profile">
-      #   #  <a href="/profiles/1/edit" rel="edit">Edit</a>
-      #   # </div>
-      #   class PirateProfile < Dill::Widget
-      #     root "#profile"
-      #
-      #     # Declare the action
-      #     action :edit, '[rel = edit]'
-      #   end
-      #
-      #   pirate_profile = widget(:pirate_profile)
-      #
-      #   # Access the action widget
-      #   action_widget = pirate_profile.widget(:edit_widget)
-      #   action_widget = pirate_profile.edit_widget
-      #
-      #   # Click the link
-      #   pirate_profile.edit
-      #
-      # @param name the name of the action
-      # @param selector the selector for the widget that will be clicked
-      def self.action(name, selector)
-        wname = :"#{name}_widget"
+    # Defines a new action.
+    #
+    # This is a shortcut to help defining a widget and a method that clicks
+    # on that widget. You can then send a widget instance the message given
+    # by +name+.
+    #
+    # You can access the underlying widget by appending "_widget" to the
+    # action name.
+    #
+    # @example
+    #   # Consider the widget will encapsulate the following HTML
+    #   #
+    #   # <div id="profile">
+    #   #  <a href="/profiles/1/edit" rel="edit">Edit</a>
+    #   # </div>
+    #   class PirateProfile < Dill::Widget
+    #     root "#profile"
+    #
+    #     # Declare the action
+    #     action :edit, '[rel = edit]'
+    #   end
+    #
+    #   pirate_profile = widget(:pirate_profile)
+    #
+    #   # Access the action widget
+    #   action_widget = pirate_profile.widget(:edit_widget)
+    #   action_widget = pirate_profile.edit_widget
+    #
+    #   # Click the link
+    #   pirate_profile.edit
+    #
+    # @param name the name of the action
+    # @param selector the selector for the widget that will be clicked
+    def self.action(name, selector)
+      wname = :"#{name}_widget"
 
-        widget wname, selector
+      widget wname, selector
 
-        define_method name do
-          widget(wname).click
+      define_method name do
+        widget(wname).click
 
-          self
-        end
+        self
       end
+    end
 
-      # Declares a new child widget.
-      #
-      # Child widgets are accessible inside the container widget using the
-      # {#widget} message, or by sending a message +name+. They
-      # are automatically scoped to the parent widget's root node.
-      #
-      # @example Defining a widget
-      #   # Given the following HTML:
-      #   #
-      #   # <div id="root">
-      #   #   <span id="child">Child</span>
-      #   # </div>
-      #   class Container < Dill::Widget
-      #     root '#root'
-      #
-      #     widget :my_widget, '#child'
-      #   end
-      #
-      #   container = widget(:container)
-      #
-      #   # accessing using #widget
-      #   my_widget = container.widget(:my_widget)
-      #
-      #   # accessing using #my_widget
-      #   my_widget = container.my_widget
-      #
-      # @overload widget(name, selector, type = Widget)
-      #
-      #   The most common form, it allows you to pass in a selector as well as a
-      #   type for the child widget. The selector will override +type+'s
-      #   root selector, if +type+ has one defined.
-      #
-      #   @param name the child widget's name.
-      #   @param selector the child widget's selector. You can pass either a
-      #     String or, if you want to use a composite selector, an Array.
-      #   @param type the child widget's parent class.
-      #
-      # @overload widget(name, type)
-      #
-      #   This form allows you to omit +selector+ from the arguments. It will
-      #   reuse +type+'s root selector.
-      #
-      #   @param name the child widget's name.
-      #   @param type the child widget's parent class.
-      #
-      #   @raise ArgumentError if +type+ has no root selector defined.
-      #
-      # @yield A block allowing you to further customize the widget behavior.
-      #
-      # @see #widget
-      def self.widget(name, *rest, &block)
-        raise ArgumentError, "`#{name}' is a reserved name" \
-          if WidgetContainer.instance_methods.include?(name.to_sym)
+    # Declares a new child widget.
+    #
+    # Child widgets are accessible inside the container widget using the
+    # {#widget} message, or by sending a message +name+. They
+    # are automatically scoped to the parent widget's root node.
+    #
+    # @example Defining a widget
+    #   # Given the following HTML:
+    #   #
+    #   # <div id="root">
+    #   #   <span id="child">Child</span>
+    #   # </div>
+    #   class Container < Dill::Widget
+    #     root '#root'
+    #
+    #     widget :my_widget, '#child'
+    #   end
+    #
+    #   container = widget(:container)
+    #
+    #   # accessing using #widget
+    #   my_widget = container.widget(:my_widget)
+    #
+    #   # accessing using #my_widget
+    #   my_widget = container.my_widget
+    #
+    # @overload widget(name, selector, type = Widget)
+    #
+    #   The most common form, it allows you to pass in a selector as well as a
+    #   type for the child widget. The selector will override +type+'s
+    #   root selector, if +type+ has one defined.
+    #
+    #   @param name the child widget's name.
+    #   @param selector the child widget's selector. You can pass either a
+    #     String or, if you want to use a composite selector, an Array.
+    #   @param type the child widget's parent class.
+    #
+    # @overload widget(name, type)
+    #
+    #   This form allows you to omit +selector+ from the arguments. It will
+    #   reuse +type+'s root selector.
+    #
+    #   @param name the child widget's name.
+    #   @param type the child widget's parent class.
+    #
+    #   @raise ArgumentError if +type+ has no root selector defined.
+    #
+    # @yield A block allowing you to further customize the widget behavior.
+    #
+    # @see #widget
+    def self.widget(name, *rest, &block)
+      raise ArgumentError, "`#{name}' is a reserved name" \
+        if WidgetContainer.instance_methods.include?(name.to_sym)
 
-        case rest.first
-        when Class
-          arg_count = rest.size + 1
-          raise ArgumentError, "wrong number of arguments (#{arg_count} for 2)" \
-            unless arg_count == 2
+      case rest.first
+      when Class
+        arg_count = rest.size + 1
+        raise ArgumentError, "wrong number of arguments (#{arg_count} for 2)" \
+          unless arg_count == 2
 
-          type = rest.first
+        type = rest.first
+        raise TypeError, "can't convert `#{type}' to Widget" \
+          unless type.methods.include?(:selector)
+        raise ArgumentError, "missing root selector for `#{type}'" \
+          unless type.selector
+
+        selector = type.selector
+      when String, Array
+        arg_count = rest.size + 1
+
+        case arg_count
+        when 0, 1
+          raise ArgumentError, "wrong number of arguments (#{arg_count} for 2)"
+        when 2
+          selector, type = [*rest, Widget]
+        when 3
+          selector, type = rest
+
           raise TypeError, "can't convert `#{type}' to Widget" \
-            unless type.methods.include?(:selector)
-          raise ArgumentError, "missing root selector for `#{type}'" \
-            unless type.selector
-
-          selector = type.selector
-        when String, Array
-          arg_count = rest.size + 1
-
-          case arg_count
-          when 0, 1
-            raise ArgumentError, "wrong number of arguments (#{arg_count} for 2)"
-          when 2
-            selector, type = [*rest, Widget]
-          when 3
-            selector, type = rest
-
-            raise TypeError, "can't convert `#{type}' to Widget" \
-              unless Class === type
-          else
-            raise ArgumentError, "wrong number of arguments (#{arg_count} for 3)"
-          end
+            unless Class === type
         else
-          raise ArgumentError, "unknown method signature: #{rest.inspect}"
+          raise ArgumentError, "wrong number of arguments (#{arg_count} for 3)"
         end
-
-        child = WidgetClass.new(selector, type, &block)
-
-        const_set(Dill::WidgetName.new(name).to_sym, child)
-
-        define_method name do
-          widget(name)
-        end
+      else
+        raise ArgumentError, "unknown method signature: #{rest.inspect}"
       end
 
-      # Creates a delegator for one child widget message.
-      #
-      # Since widgets are accessed through {WidgetContainer#widget}, we can't
-      # use {Forwardable} to delegate messages to widgets.
-      #
-      # @param name the name of the receiver child widget
-      # @param widget_message the name of the message to be sent to the child widget
-      # @param method_name the name of the delegator. If +nil+ the method will
-      #   have the same name as the message it will send.
-      def self.widget_delegator(name, widget_message, method_name = nil)
-        method_name = method_name || widget_message
+      child = WidgetClass.new(selector, type, &block)
 
-        class_eval <<-RUBY
-          def #{method_name}(*args)
-            if args.size == 1
-              widget(:#{name}).#{widget_message} args.first
-            else
-              widget(:#{name}).#{widget_message} *args
-            end
+      const_set(Dill::WidgetName.new(name).to_sym, child)
+
+      define_method name do
+        widget(name)
+      end
+    end
+
+    # Creates a delegator for one child widget message.
+    #
+    # Since widgets are accessed through {WidgetContainer#widget}, we can't
+    # use {Forwardable} to delegate messages to widgets.
+    #
+    # @param name the name of the receiver child widget
+    # @param widget_message the name of the message to be sent to the child widget
+    # @param method_name the name of the delegator. If +nil+ the method will
+    #   have the same name as the message it will send.
+    def self.widget_delegator(name, widget_message, method_name = nil)
+      method_name = method_name || widget_message
+
+      class_eval <<-RUBY
+        def #{method_name}(*args)
+          if args.size == 1
+            widget(:#{name}).#{widget_message} args.first
+          else
+            widget(:#{name}).#{widget_message} *args
           end
-        RUBY
-      end
+        end
+      RUBY
+    end
 
     # @!endgroup
 
