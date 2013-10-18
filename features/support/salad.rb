f4f68fff5c9cf3fb54651b15dcd940d648b41082
ACRE <acre@mojotech.com>
2013-10-18 21:15:05 +0000

    Extending special variables.
    2 files were modified.

--- features/support/salad.rb
+++ features/support/salad.rb
@@ -2,7 +2,7 @@ dir         = File.dirname(__FILE__)
 project_dir = File.join(dir, %w(.. ..))
 lib_dir     = File.join(project_dir, 'lib')
 
-$:.unshift File.expand_path lib_dir
+$LOAD_PATH.unshift File.expand_path lib_dir
 
 require 'dill/cucumber'
 
