65e362061099a4a6239943835895ff5e536598bb
David Leal <dleal@mojotech.com>
2014-06-20 20:15:06 +0100

    [tests] Allow defining the html we're working with.

--- features/support/app.rb
+++ features/support/app.rb
@@ -3,6 +3,24 @@ require 'sinatra/base'
 
 class DillApp < Sinatra::Base; end
 
+module WebApp
+  def define_page_body(html, path = "/test")
+    define_page <<-HTML, path
+      <html>
+      <body>
+        #{html}
+      </body>
+      </html>
+    HTML
+  end
+
+  def define_page(html, path = "/test")
+    DillApp.get(path) { html }
+  end
+end
+
+World WebApp
+
 After do
   DillApp.reset!
 end
