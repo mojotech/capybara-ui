13c5322134607d11a58bdfb5b4833d7d41b6ece4
David Leal <dleal@mojotech.com>
2014-07-02 18:25:05 +0100

    [tests] Clean up after a widget test is done.

--- features/support/widget.rb
+++ features/support/widget.rb
@@ -4,6 +4,18 @@ $LOAD_PATH.unshift lib_path
 require 'dill/cucumber'
 
 module Widget
+  def self.defined_widgets
+    @defined_widgets ||= []
+  end
+
+  def clear_widgets
+    Widget.defined_widgets.each do |e|
+      self.class.send :remove_const, e
+    end
+
+    Widget.defined_widgets.clear
+  end
+
   def define_widget(code)
     parent = self.class
     before_consts = parent.constants.dup
@@ -15,6 +27,8 @@ module Widget
     constants = after_consts - before_consts
     fail "no widget defined" if constants.size == 0
     fail "more than one widget defined" if constants.size > 1
+
+    Widget.defined_widgets << constants.first
 
     parent.const_get(constants.first)
   end
@@ -30,3 +44,7 @@ module Widget
 end
 
 World Widget
+
+After do
+  clear_widgets
+end
