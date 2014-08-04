63f1c8624d66e0e2be1ea3aba9874603a4ec3a72
David Leal <dleal@mojotech.com>
2014-08-04 20:47:31 +0100

    [doc] Tweak source code docs.

--- lib/dill/widgets/dsl.rb
+++ lib/dill/widgets/dsl.rb
@@ -1,6 +1,8 @@
 module Dill
   module Widgets
     module DSL
+      # Declares a new form widget.
+      #
       # See features/role.feature.
       def form(name, *rest, &block)
         widget name, *rest, Dill::Form, &block
@@ -8,54 +10,7 @@ module Dill
 
       # Declares a new child widget.
       #
-      # Child widgets are accessible inside the container widget using the
-      # {#widget} message, or by sending a message +name+. They
-      # are automatically scoped to the parent widget's root node.
-      #
-      # @example Defining a widget
-      #   # Given the following HTML:
-      #   #
-      #   # <div id="root">
-      #   #   <span id="child">Child</span>
-      #   # </div>
-      #   class Container < Dill::Widget
-      #     root '#root'
-      #
-      #     widget :my_widget, '#child'
-      #   end
-      #
-      #   container = widget(:container)
-      #
-      #   # accessing using #widget
-      #   my_widget = container.widget(:my_widget)
-      #
-      #   # accessing using #my_widget
-      #   my_widget = container.my_widget
-      #
-      # @overload widget(name, selector, type = Widget)
-      #
-      #   The most common form, it allows you to pass in a selector as well as a
-      #   type for the child widget. The selector will override +type+'s
-      #   root selector, if +type+ has one defined.
-      #
-      #   @param name the child widget's name.
-      #   @param selector the child widget's selector. You can pass either a
-      #     String or, if you want to use a composite selector, an Array.
-      #   @param type the child widget's parent class.
-      #
-      # @overload widget(name, type)
-      #
-      #   This form allows you to omit +selector+ from the arguments. It will
-      #   reuse +type+'s root selector.
-      #
-      #   @param name the child widget's name.
-      #   @param type the child widget's parent class.
-      #
-      #   @raise ArgumentError if +type+ has no root selector defined.
-      #
-      # @yield A block allowing you to further customize the widget behavior.
-      #
-      # @see #widget
+      # See https://github.com/mojotech/dill/blob/master/features/roles/widget.feature
       def widget(name, *rest, &block)
         raise ArgumentError, "`#{name}' is a reserved name" \
           if WidgetParts::Container.instance_methods.include?(name.to_sym)
