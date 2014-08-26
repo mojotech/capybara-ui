a05d28fbd71fa99d001c768094d5d30d75b28963
David Leal <dleal@mojotech.com>
2014-08-26 16:57:42 +0100

    [macros] Simplify .widget macro.

--- lib/dill/widgets/dsl.rb
+++ lib/dill/widgets/dsl.rb
@@ -11,42 +11,23 @@ module Dill
       # Declares a new child widget.
       #
       # See https://github.com/mojotech/dill/blob/master/features/roles/widget.feature
-      def widget(name, *rest, &block)
+      def widget(name, selector_or_parent, parent = Widget, &block)
         raise ArgumentError, "`#{name}' is a reserved name" \
           if WidgetParts::Container.instance_methods.include?(name.to_sym)
 
-        case rest.first
-        when Class
-          arg_count = rest.size + 1
-          raise ArgumentError, "wrong number of arguments (#{arg_count} for 2)" \
-            unless arg_count == 2
-
-          type = rest.first
-          raise TypeError, "can't convert `#{type}' to Widget" \
-            unless type.methods.include?(:selector)
-          raise ArgumentError, "missing root selector for `#{type}'" \
-            unless type.selector
-
-          selector = type.selector
+        case selector_or_parent
         when String, Array, Proc
-          arg_count = rest.size + 1
-
-          case arg_count
-          when 0, 1
-            raise ArgumentError, "wrong number of arguments (#{arg_count} for 2)"
-          when 2
-            selector, type = [*rest, Widget]
-          when 3
-            selector, type = rest
-
-            raise TypeError, "can't convert `#{type}' to Widget" \
-              unless Class === type
-          else
-            raise ArgumentError, "wrong number of arguments (#{arg_count} for 3)"
-          end
+          selector, type = selector_or_parent, parent
+        when Class
+          selector, type = selector_or_parent.selector, selector_or_parent
         else
-          raise ArgumentError, "unknown method signature: #{rest.inspect}"
+          raise ArgumentError, "wrong number of arguments (#{rest.size} for 1)"
         end
+
+        raise TypeError, "can't convert `#{type}' to Widget" \
+          unless type.methods.include?(:selector)
+
+        raise ArgumentError, "missing selector" unless selector || type.selector
 
         child = WidgetClass.new(selector, type, &block)
 
