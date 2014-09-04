44d4cd9b048a9b0079404b3ea143593474534c1d
David Leal <dleal@mojotech.com>
2014-09-04 12:03:55 +0100

    [matchers] Implement waiting for negative #see.

--- lib/dill/matchers.rb
+++ lib/dill/matchers.rb
@@ -1,9 +1,17 @@
 require 'rspec/matchers'
 
 RSpec::Matchers.define :see do |widget_name, *args|
-  match do |role|
+  match_for_should do |role|
     begin
       eventually { role.see?(widget_name, *args) }
+    rescue Dill::Checkpoint::ConditionNotMet
+      false
+    end
+  end
+
+  match_for_should_not do |role|
+    begin
+      eventually { ! role.see?(widget_name, *args) }
     rescue Dill::Checkpoint::ConditionNotMet
       false
     end
