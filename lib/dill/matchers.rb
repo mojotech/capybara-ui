47d7a0ad60d38eac38e80503919cf69f5dce7262
David Leal <dleal@mojotech.com>
2014-09-04 11:33:44 +0100

    [matchers] Let #see wait.

--- lib/dill/matchers.rb
+++ lib/dill/matchers.rb
@@ -1,7 +1,11 @@
 require 'rspec/matchers'
 
 RSpec::Matchers.define :see do |widget_name, *args|
-  match  do |role|
-    role.see?(widget_name, *args)
+  match do |role|
+    begin
+      eventually { role.see?(widget_name, *args) }
+    rescue Dill::Checkpoint::ConditionNotMet
+      false
+    end
   end
 end
