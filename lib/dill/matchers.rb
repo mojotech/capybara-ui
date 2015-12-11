61b0046ebd62e6eebcbcd6fda4d60a06cfd8478e
Adam <adamgsteel@gmail.com>
2015-12-11 13:57:21 -0700

    [deps] check for RSpec dependency version if required

--- lib/dill/matchers.rb
+++ lib/dill/matchers.rb
@@ -1,5 +1,12 @@
 begin
   require 'rspec/matchers'
+  require 'rspec/version'
+
+  unless Gem::Version.new(RSpec::Version::STRING) >= Gem::Version.new(Dill::OptionalDependencies::RSPEC_VERSION)
+    raise LoadError,
+      "requires RSpec version #{Dill::OptionalDependencies::RSPEC_VERSION} or later. " \
+      "You have #{RSpec::Version::STRING}."
+  end
 
   RSpec::Matchers.define :see do |widget_name, *args|
     match do |role|
@@ -18,6 +25,4 @@ begin
       end
     end
   end
-rescue LoadError
-  # *crickets*
 end
