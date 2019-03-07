7b3c9aab8eb5bca2af7f0f4b52fafc2b09557fd6
Chris Shoemaker <chris@mojotech.com>
2019-03-07 16:23:14 -0500

    Remove deprecated `exact_options` capybara setting.

--- lib/capybara/ui/capybara.rb
+++ lib/capybara/ui/capybara.rb
@@ -1,7 +1,6 @@
 Capybara.configure do |config|
   config.match = :one
   config.exact = true
-  config.exact_options = true
   config.ignore_hidden_elements = true
   config.visible_text_only = true
 end
